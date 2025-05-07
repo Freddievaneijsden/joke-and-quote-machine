87a38984c8b9dea604f7ad52c7501d9606ec3743
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-05-07 10:56:47 +0200

    add jokeservice and quoteservice modules

---
+++ jokeservice/src/main/java/com/example/jokeservice/JokeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jokeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeserviceApplication.class, args);
+	}
+
+}
