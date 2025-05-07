61fc147ac07e7632bc7651b5fbe1c188ec949de5
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-05-07 10:58:46 +0200

    update jokeservice module

--- jokeservice/src/main/java/com/example/jokeservice/JokeserviceApplication.java
+++ jokeservice/src/main/java/com/example/jokeservice/JokeserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JokeserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JokeserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JokeserviceApplication.class, args);
+    }
 
 }
