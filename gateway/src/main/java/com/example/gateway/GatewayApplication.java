9b388e5fae8811f5e091783250a8ec7482394588
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-05-19 15:50:53 +0200

    add routes in GatewayApplication and application.yaml

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,6 +2,13 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.HandlerFunction;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -9,5 +16,14 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+//	@Bean
+//	RouterFunction<ServerResponse> routes() {
+//		return RouterFunctions.route()
+//				//
+//				.GET("/api/get", HandlerFunctions.http()) //matching
+//				.before(BeforeFilterFunctions.uri("http://localhost:8080")) //apply filter that connects to specifik computer by URI
+//				.build();
+//	}
 
 }
