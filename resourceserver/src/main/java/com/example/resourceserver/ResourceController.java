00ffce80a1bdd19467f0b3758c7c1e3f6a39fcd8
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-05-20 08:47:34 +0200

    add logging to ResourceController

--- resourceserver/src/main/java/com/example/resourceserver/ResourceController.java
+++ resourceserver/src/main/java/com/example/resourceserver/ResourceController.java
@@ -1,10 +1,15 @@
 package com.example.resourceserver;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResourceController.class);
 
 //    @CrossOrigin(origins = "http://localhost:8888") Fast way of allowing CORS instead of own config
     @GetMapping("/secure")
@@ -13,7 +18,12 @@ public class ResourceController {
     }
 
     @GetMapping("/public")
-    public String publicEndpoint() {
+    public String publicEndpoint(HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+        int clientPort = request.getRemotePort();
+
+        logger.info("Client IP: {} - Port: {}", clientIp, clientPort);
+
         return "Hello from the PUBLIC Resource Server!";
     }
 }
